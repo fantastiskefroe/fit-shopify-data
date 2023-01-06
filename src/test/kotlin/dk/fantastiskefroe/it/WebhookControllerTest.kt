15745a9cfb42e8aa98bb67ac63f2c69efad6e8ef
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-01-06 12:57:43 +0100

    Update WebhookControllerTest.kt

--- src/test/kotlin/dk/fantastiskefroe/it/WebhookControllerTest.kt
+++ src/test/kotlin/dk/fantastiskefroe/it/WebhookControllerTest.kt
@@ -3,6 +3,7 @@ package dk.fantastiskefroe.it
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
