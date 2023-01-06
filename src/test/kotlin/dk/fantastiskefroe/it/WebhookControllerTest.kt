04cc61123a90092dc5ef4cef0349bd65cb766995
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-01-06 12:51:22 +0100

    Disable test

--- src/test/kotlin/dk/fantastiskefroe/it/WebhookControllerTest.kt
+++ src/test/kotlin/dk/fantastiskefroe/it/WebhookControllerTest.kt
@@ -9,12 +9,8 @@ import org.junit.jupiter.api.Test
 class WebhookControllerTest {
 
     @Test
-    fun testHelloEndpoint() {
-//        given()
-//          .`when`().get("/hello")
-//          .then()
-//             .statusCode(200)
-//             .body(`is`("Hello from RESTEasy Reactive"))
+    @Disabled
+    fun test() {
     }
 
 }
